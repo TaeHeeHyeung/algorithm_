a1c5f60a5fa9238072285f1b442b495469717764
TaeHee_Hyeung <hth0893@naver.com>
2021-03-08 00:46:24 +0900

    Dp 9184 신나는 함수 실행 : 두 번째 메모리 초과2

--- java_test/src/main/java/com/example/lib/DP_9184_FunnyFunc.java
+++ java_test/src/main/java/com/example/lib/DP_9184_FunnyFunc.java
@@ -32,14 +32,16 @@ public  class DP_9184_FunnyFunc {
     }// end process
 
     public int w(int a, int b, int c) {
-        if (a > 20 || b > 20 || c > 20) {
-            a = 20;
-            b = 20;
-            c = 20;
-        }
         int a_save = a + WEIGHT;
         int b_save = b + WEIGHT;
         int c_save = c + WEIGHT;
+        if ((a > 20 || b > 20 || c > 20) && arr[a_save][b_save][c_save] != 0) {
+            return arr[a_save][b_save][c_save];
+        }
+
+        if ((a <= 0 || b <= 0|| c <= 0) && arr[a_save][b_save][c_save] != 0) {
+            return arr[a_save][b_save][c_save];
+        }
         if (arr[a_save][b_save][c_save] != 0) {
             return arr[a_save][b_save][c_save];
         }
@@ -53,7 +55,7 @@ public  class DP_9184_FunnyFunc {
             arr[a_save][b_save][c_save] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1)
                     - w(a - 1, b - 1, c - 1);
         }
-        //test로그
+        // test로그
 //			if (a == WEIGHT && b == WEIGHT && c == WEIGHT) {
 //				System.out.println("0,1,2: " + a + ", " + b + ", " + c + " :" + arr[a_save][b_save][c_save]);
 //			}
@@ -61,6 +63,7 @@ public  class DP_9184_FunnyFunc {
 
         return arr[a_save][b_save][c_save];
     }
+
 
     //Weight 예외처리 하여 출력
     public void print(int input, int input2, int input3) {
